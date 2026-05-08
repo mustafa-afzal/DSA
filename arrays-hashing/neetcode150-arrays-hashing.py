dd2fb04be3c2db50133f1133da2060327b5ca43c
Mustafa Afzal <mafz@umich.edu>
2026-05-08 02:40:23 -0400

    Top k freq elements

--- arrays-hashing/neetcode150-arrays-hashing.py
+++ arrays-hashing/neetcode150-arrays-hashing.py
@@ -28,6 +28,22 @@ class Solution:
 
 # Group Anagrams
 
+# Top K Freq Elements
+
+from collections import Counter
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freqNums = Counter(nums)
+        numsSorted = sorted(freqNums.items(), 
+            key = lambda x: x[1], reverse=True)
+        topKeys = []
+        for i in numsSorted[:k]:
+            key = i[0]
+            topKeys.append(key)
+        return topKeys
+
+
 # Encode and Decode Strings
 
 class Solution:
