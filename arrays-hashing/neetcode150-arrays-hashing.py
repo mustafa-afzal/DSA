55ad4cd3ffcb7cab3900cc06fb55a45ef40154cd
Mustafa Afzal <mafz@umich.edu>
2026-05-08 02:40:58 -0400

    Group Anagrams

--- arrays-hashing/neetcode150-arrays-hashing.py
+++ arrays-hashing/neetcode150-arrays-hashing.py
@@ -28,6 +28,17 @@ class Solution:
 
 # Group Anagrams
 
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        map = {}
+        for s in strs:
+            sSorted = "".join(sorted(s))
+            if sSorted in map:
+                map[sSorted].append(s)
+            else:
+                map[sSorted] = [s]
+        return (list(map.values())) 
+
 # Top K Freq Elements
 
 from collections import Counter
