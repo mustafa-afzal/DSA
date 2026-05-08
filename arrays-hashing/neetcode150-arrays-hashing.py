fd431634c9c4941420fd112096d432086bde927f
Mustafa Afzal <mafz@umich.edu>
2026-05-08 02:39:43 -0400

    Encode Decode Strings

--- arrays-hashing/neetcode150-arrays-hashing.py
+++ arrays-hashing/neetcode150-arrays-hashing.py
@@ -25,3 +25,29 @@ class Solution:
             if comp in map:
                 return [i, map[comp]]
             map[v] = i
+
+# Group Anagrams
+
+# Encode and Decode Strings
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        s = ""
+        for i in strs:
+            s += str(len(i)) + "#" + i
+        return s
+      
+    def decode(self, s: str) -> List[str]:
+        res = []
+        i = 0
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+            length = int(s[i:j])
+            i = j + 1
+            j = i + length - 1
+            res.append(s[i:j+1])
+            i = j + 1
+        return res
