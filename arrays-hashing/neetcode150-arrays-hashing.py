d4c1b6e7d1f2665eb4607d41fbe6b3040e203f77
Mustafa Afzal <mafz@umich.edu>
2026-05-08 18:57:41 -0400

    longest consecutive sequence

--- arrays-hashing/neetcode150-arrays-hashing.py
+++ arrays-hashing/neetcode150-arrays-hashing.py
@@ -116,3 +116,18 @@ class Solution:
                 squares[r // 3, c // 3].add(board[r][c])
         return True
         
+# Longest Consecutive Sequence
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numsSet = set(nums)
+        maxLen = 0
+        for i in numsSet:
+            if i - 1 not in numsSet:
+                j = i
+                length = 1
+                while i + length in numsSet:
+                    length += 1
+                maxLen = max(length, maxLen)
+        return maxLen
+            
