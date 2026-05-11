a0d79b13edebb53104e005a41bb73040a35dd3f8
Mustafa Afzal <mafz@umich.edu>
2026-05-11 15:09:28 -0400

    3Sum

--- arrays-hashing/neetcode150-two-pointers.py
+++ arrays-hashing/neetcode150-two-pointers.py
@@ -28,3 +28,29 @@ class Solution:
                 l += 1
             else:
                 r -= 1
+
+# 3Sum 
+
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        nums = sorted(nums)
+        res = []
+        r = len(nums) - 1
+        for i in range(0, r):
+            l, r = i + 1, len(nums) - 1
+            if i != 0 and nums[i] == nums[i-1]:
+                continue
+            while l < r:
+                if nums[i] + nums[l] + nums[r] == 0:
+                    res.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while nums[l] == nums[l-1] and l < r:
+                        l += 1
+                elif nums[i] + nums[l] + nums[r] > 0:
+                    r -= 1
+                else :
+                    l += 1
+        return res
+
+        
