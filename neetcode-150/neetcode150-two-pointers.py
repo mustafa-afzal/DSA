64d224c043906530030222eeac92c3cf2398fbaa
Mustafa Afzal <mafz@umich.edu>
2026-05-13 14:41:38 -0400

    Trapping Rain Water

--- neetcode-150/neetcode150-two-pointers.py
+++ neetcode-150/neetcode150-two-pointers.py
@@ -53,4 +53,25 @@ class Solution:
                     l += 1
         return res
 
+# Trapping Rain Water
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        l, r = 0, len(height) - 1
+        leftMax = height[l]
+        rightMax = height[r]
+        trapped = 0
+        while l <= r:
+            if leftMax <= rightMax:
+                leftMax = max(leftMax, height[l])
+                trapped += leftMax - height[l]
+                l += 1
+            else:
+                rightMax = max(rightMax, height[r])
+                trapped += rightMax - height[r]
+                r -= 1
+        return trapped
+
+
+
         
