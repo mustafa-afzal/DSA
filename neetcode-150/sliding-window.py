002440e30559458a2efcdffa07d61f777b991209
Mustafa Afzal <mafz@umich.edu>
2026-05-14 15:03:18 -0400

    Best time to buy and sell stock

---
+++ neetcode-150/sliding-window.py
@@ -0,0 +1,11 @@
+# Best Time to Buy and Sell Stock
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxP = 0
+        cheapest = prices[0]
+        for i in range(len(prices)):
+            cheapest = min(prices[i], cheapest)
+            profit = prices[i] - cheapest
+            maxP = max(profit, maxP)
+        return maxP
