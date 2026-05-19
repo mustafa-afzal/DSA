e854e2a1cf443f24ce04ce935e37344b904460cb
Mustafa Afzal <mafz@umich.edu>
2026-05-18 20:19:29 -0400

    longest repeating char replacement

--- neetcode-150/sliding-window.py
+++ neetcode-150/sliding-window.py
@@ -24,6 +24,28 @@ class Solution:
             charSet.add(s[r])
             res = max(res, r - l + 1)
         return res
+    
+# Longest Repeating Character Replacement
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l = 0
+        res = 0
+        dict1 = {}
+        for r in range(len(s)):
+            if s[r] in dict1:
+                dict1[s[r]] += 1
+            else:
+                dict1[s[r]] = 1
+            windowSize = r - l + 1
+            maxFreq = max(dict1.values())
+            if windowSize - maxFreq > k:
+                dict1[s[l]] -= 1
+                l += 1
+            else:
+                res = max(res, windowSize)
+        return res
+
 
 
 
