20f03f1fa385165f75ab201fa4d93a9f16e7929a
Mustafa Afzal <mafz@umich.edu>
2026-05-21 13:02:43 -0400

    sliding window max

--- neetcode-150/sliding-window.py
+++ neetcode-150/sliding-window.py
@@ -108,6 +108,24 @@ class Solution:
         if minWindowLen == float('inf'):
             return ""
         return s[resL : resR + 1]
+    
+# Sliding Window Maximum
+
+from collections import deque
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        d = deque()
+        res = []
+        for i in range(len(nums)):
+            while d and nums[d[-1]] < nums[i]:
+                d.pop()
+            d.append(i)
+            if d[0] < i - k + 1:
+                d.popleft()
+            if i >= k - 1:
+                res.append(nums[d[0]])
+        return res            
                 
             
         
