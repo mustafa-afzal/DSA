3952c8b496d1ad5d3d729742f77490ecfa5cafd6
Mustafa Afzal <mafz@umich.edu>
2026-06-24 16:43:58 -0400

    reverse polish notation

--- neetcode-150/stack.py
+++ neetcode-150/stack.py
@@ -13,5 +13,70 @@ class Solution:
 
 # Min Stack
 
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, value: int) -> None:
+        self.stack.append(value)
+        if (self.minStack and value >= self.minStack[-1]):
+            self.minStack.append(self.minStack[-1])
+        else:
+            self.minStack.append(value)
+        
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(value)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+
+# Evaluate Reverse Polish Notation
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        res = int(tokens[0])
+        for i in range(len(tokens)):
+            if tokens[i] == "+":
+                res = stack[-2] + stack[-1]
+                stack.pop()
+                stack.pop()
+                stack.append(res)
+            elif tokens[i] == "-":
+                res = stack[-2] - stack[-1]
+                stack.pop()
+                stack.pop()
+                stack.append(res)
+            elif tokens[i] == "*":
+                res = stack[-2] * stack[-1]
+                stack.pop()
+                stack.pop()
+                stack.append(res)
+            elif tokens[i] == "/":
+                res = int(stack[-2] / stack[-1])
+                stack.pop()
+                stack.pop()
+                stack.append(res)
+            else:
+                stack.append(int(tokens[i]))
+        return res
 
 
