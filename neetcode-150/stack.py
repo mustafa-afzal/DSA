9b8dc55b74e51cfd94a9eb5434d047203ee154ce
Mustafa Afzal <mafz@umich.edu>
2026-06-25 23:58:57 -0400

    Daily Temperatures

--- neetcode-150/stack.py
+++ neetcode-150/stack.py
@@ -79,4 +79,17 @@ class Solution:
                 stack.append(int(tokens[i]))
         return res
 
+# Daily Temperatures
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        answer = [0] * len(temperatures)
+        stack = []
+        for i in range(len(temperatures)):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                answer[stack[-1]] = i - stack[-1]
+                stack.pop()
+            stack.append(i)
+        return answer
+
 
