28268a11f166062e455e9aa4deca86efee7817b9
Mustafa Afzal <mafz@umich.edu>
2026-06-26 15:41:42 -0400

    Car Fleet

--- neetcode-150/stack.py
+++ neetcode-150/stack.py
@@ -92,4 +92,23 @@ class Solution:
             stack.append(i)
         return answer
 
+# Car Fleet
+
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pairs = sorted(zip(position, speed), reverse=True)
+        stack = []
+        for p, s in pairs:
+            time = (target - p) / s
+            stack.append(time)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
+            
+            
+
+
+
+
+
 
