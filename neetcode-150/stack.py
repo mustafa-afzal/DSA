13fd67fb6d0d1798f64acf2958cce6c7d1a03274
Mustafa Afzal <mafz@umich.edu>
2026-06-29 17:34:41 -0400

    largest rect in histogram

--- neetcode-150/stack.py
+++ neetcode-150/stack.py
@@ -104,6 +104,24 @@ class Solution:
             if len(stack) >= 2 and stack[-1] <= stack[-2]:
                 stack.pop()
         return len(stack)
+
+# Largest Rectangle in Histogram
+
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        maxA = 0
+        stack = []
+        for i, h in enumerate(heights):
+            start = i
+            while stack and stack[-1][1] > h:
+                index, height = stack.pop()
+                maxA = max(maxA, height * (i - index))
+                start = index
+            stack.append((start, h))
+        
+        for i, h in stack:
+            maxA = max(maxA, h * (len(heights) - i))
+        return maxA
             
             
 
