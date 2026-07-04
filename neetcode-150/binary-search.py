8c241d1ac0a56112ec39faa528e0da92dbe44437
Mustafa Afzal <mafz@umich.edu>
2026-07-04 13:08:41 -0400

    koko eating bananas

--- neetcode-150/binary-search.py
+++ neetcode-150/binary-search.py
@@ -56,3 +56,23 @@ class Solution:
             else:
                 return True
         return False
+
+# Koko Eating Bananas
+
+import math
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)
+        minK = max(piles)
+        while l <= r:
+            mid = (l + r) // 2
+            pileTime = 0
+            for i in range(len(piles)):
+                pileTime += math.ceil(piles[i] / mid)
+            if pileTime <= h:
+                minK = min(mid, minK)
+                r = mid - 1
+            else:
+                l = mid + 1
+        return minK
