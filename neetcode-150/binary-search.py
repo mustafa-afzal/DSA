8b198e7fe78e95773fb59f7fbfc171fc8e0760a6
Mustafa Afzal <mafz@umich.edu>
2026-07-09 14:40:29 -0400

    find min in rotated sorted arr

--- neetcode-150/binary-search.py
+++ neetcode-150/binary-search.py
@@ -76,3 +76,22 @@ class Solution:
             else:
                 l = mid + 1
         return minK
+
+# Find Minimum in Rotated Sorted Array
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        res = nums[0]
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            if nums[l] < nums[r]:
+                res = min(nums[l], res)
+                break
+            
+            m = (l + r) // 2
+            res = min(nums[m], res)
+            if nums[m] >= nums[l]:
+                l = m + 1
+            else:
+                r = m - 1
+        return res
