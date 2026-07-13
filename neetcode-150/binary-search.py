b3e601994c843dc21b0509e6011972f4fab1835c
Mustafa Afzal <mafz@umich.edu>
2026-07-13 16:18:35 -0400

    search in rotated sorted array

--- neetcode-150/binary-search.py
+++ neetcode-150/binary-search.py
@@ -95,3 +95,24 @@ class Solution:
             else:
                 r = m - 1
         return res
+
+# Search in Rotated Sorted Array
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            elif nums[m] > nums[r]:
+                if target <= nums[m] and target >= nums[l]:
+                    r = m - 1
+                else:
+                    l = m + 1
+            elif nums[m] <= nums[r]:
+                if target >= nums[m] and target <= nums[r]:
+                    l = m + 1
+                else:
+                    r = m - 1
+        return -1
