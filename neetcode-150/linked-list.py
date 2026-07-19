c78a047cca43ddfe43c1a006b4cbc5cc7b2ca058
Mustafa Afzal <mafz@umich.edu>
2026-07-19 12:37:21 -0400

    Linked List Cycle

--- neetcode-150/linked-list.py
+++ neetcode-150/linked-list.py
@@ -37,3 +37,21 @@ class Solution:
                 tail = tail.next
         tail.next = list1 if list1 else list2
         return dummy.next
+
+# Linked List Cycle
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
