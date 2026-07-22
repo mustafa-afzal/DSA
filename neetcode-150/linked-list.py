2b16dbe59addfdbb80a94cbf7c54d7a2d009f5c0
Mustafa Afzal <mafz@umich.edu>
2026-07-22 13:50:56 -0400

    copy list with random pointer

--- neetcode-150/linked-list.py
+++ neetcode-150/linked-list.py
@@ -115,5 +115,30 @@ class Solution:
         curr.next = curr.next.next 
         return head
 
+# Copy List with Random Pointer
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        curr = head
+        map = {}
+        while curr:
+            map[curr] = Node(curr.val)
+            curr = curr.next
+        curr = head
+        while curr:
+            map[curr].next = map[curr.next] if curr.next else None
+            map[curr].random = map[curr.random] if curr.random else None
+            curr = curr.next
+        return map[head] if head else None
+
 
     
