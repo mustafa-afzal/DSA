3597aa884abcfb57cc2ba014918fa90fd8af4e89
Mustafa Afzal <mafz@umich.edu>
2026-07-23 13:01:30 -0400

    container with most water

--- neetcode-150/two-pointers.py
+++ neetcode-150/two-pointers.py
@@ -53,6 +53,22 @@ class Solution:
                     l += 1
         return res
 
+# Container with Most Water
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l, r = 0, len(height) - 1
+        maxA = 0
+        while l < r:
+            area = min(height[l], height[r]) * (r - l)
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+            maxA = max(area, maxA)
+        return maxA
+
+
 # Trapping Rain Water
 
 class Solution:
