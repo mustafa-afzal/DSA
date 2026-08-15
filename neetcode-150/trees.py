0c2a95a8b7c42a0cbd8dbe7eefa5af392344ac44
Mustafa Afzal <mafz@umich.edu>
2026-08-15 13:07:57 -0400

    same tree

--- neetcode-150/trees.py
+++ neetcode-150/trees.py
@@ -76,5 +76,29 @@ class Solution:
             balanced = (left[0] and right[0] and abs(left[1] - right[1]) <= 1)
             return [balanced, 1 + max(left[1], right[1])]
         return dfs(root)[0]
+
+# Same Tree
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        
+        def dfs(pNode, qNode):
+            if not pNode and not qNode:
+                return True
+            if not pNode or not qNode:
+                return False
+            if pNode.val != qNode.val:
+                return False
+            left = dfs(pNode.left, qNode.left)
+            right = dfs(pNode.right, qNode.right)
+            return left and right
+        return dfs(p, q)
+
         
                
